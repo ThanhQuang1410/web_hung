4f3be3c6470c2306d916c556336d568ea36d812d
ThanhQuang1410 <ttquang1410@gmail.com>
2018-06-04 16:09:17 +0700

    last commit

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
  <?php
 $servername = "localhost";
 $username = "root";
-$password = "23697";
+$password = "";
 $dbname = "hatstore";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
